f003a3ed7cb87b6a29b3d15f84260a2c9d988803
Evelyn Arequipa <evelyn.arequipa7145@utc.edu.ec>
2025-06-09 08:08:17 -0500

    Ingresamos el nombre de nuestra aplicacion

--- soportetec/settings.py
+++ soportetec/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Aplicaciones.Equipo'
+    'Aplicaciones.SistemaOperativo'
+    
+    
 ]
 
 MIDDLEWARE = [
