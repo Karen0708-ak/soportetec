ada03e835f5e8a87682bc6b6bb79ac1ae4ed4e16
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-09 09:24:13 -0500

    Se se agrego el inicio para la aplicacion Equipo

--- Aplicaciones/Equipo/views.py
+++ Aplicaciones/Equipo/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import SistemaOperativo
+from .models import Equipo
+from django.contrib import messages
 # Create your views here.
+
+def inicio(request):
+    listadoEquipo=Equipo.objects.all()
+    return render(request,"inicioeqtml",{'equipo':listadoEquipo})
+def nuevoEquipo(request):
+    rsistema_operatvo=SistemaOperativo.objects.all()
+    return render(request,"nuevoMuestreo.html",{'sistema_operativo':rsistema_operatvo})
+
+def guardarEquipo(request):
+    nombre = request.POST["nombre"]
+    marca = request.POST["marca"]
+    modelo = request.POST["modelo"]
+    procesador = request.POST["procesador"]
+    sistema_operativoid = request.POST["sistema_operativo"]
+    sistema_operativo=SistemaOperativo.objects.get(id=sistema_operativoid)
+     #Subiendo archivo
+    fotoe=request.FILES.get("fotoe")
+    #Archivo
+    fichatec=request.FILES.get("fichatec")
+    nuevoEquipo=Equipo.objects.create(
+            nombre=nombre,
+            marca=marca,
+            modelo=modelo,
+            procesador=procesador,
+            fotoe=fotoe,
+            fichatec=fichatec,
+            sistema_operativo=sistema_operativo,
+        )
+    #mensaje de confirmacion
+    messages.success(request,"Equipo guardado exitosamente")
+    return redirect('inicioeq')
