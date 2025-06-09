eaf778505f0768dc050b33496d70de54274f95ab
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-09 08:55:05 -0500

    Se configurararon las rutas para la aplicacion equipo

--- Aplicaciones/Equipo/models.py
+++ Aplicaciones/Equipo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from Aplicaciones.SistemaOperativo.models import SistemaOperativo
 
 # Create your models here.
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=100)
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    procesador = models.CharField(max_length=50)
+    fotoe = models.FileField(upload_to='equipos')
+    fichatec = models.FileField(upload_to='equipos')
+    sistema_operativo = models.ForeignKey(SistemaOperativo, on_delete=models.CASCADE)
