42ed7bda262c14d9a309ac85016968b7f676c5c5
Evelyn Arequipa <evelyn.arequipa7145@utc.edu.ec>
2025-06-09 09:13:21 -0500

    Creamos una carpeta para guardar los documentos, tambien ingresamos un nuevo atributo

--- Aplicaciones/SistemaOperativo/models.py
+++ Aplicaciones/SistemaOperativo/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class SistemaOperativo(models.Model):
     nombre = models.CharField(max_length=100)
     version = models.CharField(max_length=50)
+    documentacion = models.FileField(upload_to='documentos_so/', null=True, blank=True)
 
     def __str__(self):
         return f"{self.nombre} {self.version}"
