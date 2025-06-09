5bfb2dda75ceefc057cc65dbf1038e34819d56b1
Evelyn Arequipa <evelyn.arequipa7145@utc.edu.ec>
2025-06-09 08:25:01 -0500

    Ingresamos el nombre de la aplicacion en apps.py

--- Aplicaciones/SistemaOperativo/apps.py
+++ Aplicaciones/SistemaOperativo/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SistemaoperativoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SistemaOperativo'
+    name = 'Aplicaciones.SistemaOperativo'
